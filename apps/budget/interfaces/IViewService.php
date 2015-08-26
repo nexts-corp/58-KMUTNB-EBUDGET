07856ec8ba0a2396be920dfa3afa4d9f1615dd03
palida <palida.p@baanprogram.com>
2015-08-26 09:46:06 +0700

    fix conflict in manifest.server.json

---
+++ apps/budget/interfaces/IViewService.php
@@ -0,0 +1,69 @@
+<?php
+namespace apps\budget\interfaces;
+
+/**
+ * @name ViewService
+ * @uri /view
+ * @description ViewService
+ */
+interface IViewService {
+   
+    /**
+     * @name formBudget
+     * @uri /formBudget
+     * @description ยื่นคำของบประมาณ
+     */ 
+    public function formBudget();
+    
+    /**
+     * @name bg140
+     * @uri /bg140
+     * @description แบบ ง.140
+     */ 
+    public function bg140();
+    
+    /**
+     * @name bg141
+     * @uri /bg141
+     * @description แบบ ง.141
+     */ 
+    public function bg141();
+    
+    /**
+     * @name bg142
+     * @uri /bg142
+     * @description แบบ ง.142
+     */ 
+    public function bg142();
+    
+    /**
+     * @name bg143
+     * @uri /bg143
+     * @description แบบ ง.143
+     */ 
+    public function bg143();
+    
+    /**
+     * @name bg144
+     * @uri /bg144
+     * @description แบบ ง.144
+     */ 
+    public function bg144();
+    
+    /**
+     * @name bg145
+     * @uri /bg145
+     * @description แบบ ง.145
+     */ 
+    public function bg145();
+    
+    /**
+     * @name bg146
+     * @uri /bg146
+     * @description แบบ ง.146
+     */ 
+    public function bg146();
+    
+    
+    
+}
---
+++ apps/budget/service/WarehouseInfoService.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace apps\budget\service;
+
+
+use apps\budget\interfaces\IWarehouseInfoService;
+use th\co\bpg\cde\core\CServiceBase;
+use th\co\bpg\cde\data\CDataContext;
+
+class WarehouseInfoService extends CServiceBase implements IWarehouseInfoService{
+    public $datacontext;
+    public $logger;
+    public $md = "apps\\common\\model";
+    public $ent = "apps\\common\\entity";
+
+    public function __construct() {
+        $this->logger = \Logger::getLogger("root");
+        $this->datacontext = new CDataContext(NULL);
+    }
+    
+    public function listsRice(){
+        $sql = "SELECT"
+                ." ri.id, ri.provinceId, pv.provinceNameTH, ri.silo, ac.associate,"
+                ." ri.warehouse, ri.stack, ri.projectId, pj.project, ri.typeId, tp.type, ri.gradeId, gd.grade, ri.discountRate, ri.statusKeyword, st.status"
+            ." FROM ".$this->ent."\\RiceInfo ri"
+            ." JOIN ".$this->ent."\\Province pv WITH pv.id = ri.provinceId"
+            ." JOIN ".$this->ent."\\Associate ac WITH ac.id = ri.associateId"
+            ." JOIN ".$this->ent."\\Project pj WITH pj.id = ri.projectId"
+            ." JOIN ".$this->ent."\\Type tp WITH tp.id = ri.typeId"
+            ." JOIN ".$this->ent."\\Grade gd WITH gd.id = ri.gradeId"
+            ." LEFT JOIN ".$this->ent."\\Status st WITH st.keyword = ri.statusKeyword";
+        $data = $this->datacontext->getObject($sql);
+        
+        /*$projArr = array();
+        $provArr = array();
+        $typeArr = array();
+        $siloArr = array();
+        $gradeArr = array();
+        $statusArr = array();
+        
+        foreach ($data as $key => $value) {
+            $provArr[$value["provinceId"]] = $value["provinceNameTH"];
+            $projArr[$value["projectId"]] = $value["project"];
+            $typeArr[$value["typeId"]] = $value["type"];
+            $siloArr[$value["silo"]] = $value["silo"];
+            $gradeArr[$value["gradeId"]] = $value["grade"];
+            $statusArr[$value["statusKeyword"]] = $value["status"];
+        }
+        
+        $this->getResponse()->add("province", $provArr);
+        $this->getResponse()->add("project", $projArr);
+        $this->getResponse()->add("type", $typeArr);
+        $this->getResponse()->add("silo", $siloArr);
+        $this->getResponse()->add("grade", $gradeArr);
+        $this->getResponse()->add("status", $statusArr);*/
+        
+        return $data;
+    }
+} 
