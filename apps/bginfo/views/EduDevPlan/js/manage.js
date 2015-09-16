9b6154380ede9d0830ac3e93960d33fe79f475b6
Pongpanot Tassaneeyachumphalee <pongpanot.t@baanprogram.com>
2015-09-16 19:54:36 +0700

    Edit bginfo and report

--- apps/bginfo/views/EduDevPlan/js/manage.js
+++ apps/bginfo/views/EduDevPlan/js/manage.js
@@ -2,18 +2,15 @@ var myApp = angular.module('EduDevPlan', ['commonApp']);
 
 myApp.controller('mainController', function($scope,$http,$controller) {
     $controller('cmListController', {$scope: $scope});
-
+    
     $scope.init = function () {
         $scope.cmListYear();
         
-        $scope.dataBudgetType = [];
-        $scope.idLevel  = [];
-        $scope.nameLevel  = [];
-        $scope.nametextLevel  = "";
-        
-        
-        
+        $scope.dataIssueAndTarget = []; 
+        $scope.pageLevel = 1;
+        $scope.fetchType();
     };
+    
     
     
     $scope.findIndexObject = function(obj, attr, value){
@@ -24,16 +21,11 @@ myApp.controller('mainController', function($scope,$http,$controller) {
         }
     };
     
-    $scope.findValueArray = function(arr,value){
-        var check = 0;
-        for(var i = 0; i < arr.length; i += 1) {
-            
-            if(arr[i] === value) {
-                check = 1;
-            }
-        }
-        return check;
+    
+    $scope.firstLetterUpperCase = function(value){
+      return value.substr(0, 1).toUpperCase() + value.substr(1);
     };
+    
     
     $scope.centerModal = function(){
             $('.modal-content').modal().css({
@@ -43,92 +35,194 @@ myApp.controller('mainController', function($scope,$http,$controller) {
             });
     };
     
-    $scope.modal = function(action,editId,name) {
+   
+   
+   
+   
+    $scope.pageIssueAndTarget = function(){
+        $scope.pageLevel = 1;
+    };
+   
+   $scope.pageKpiAndStrategy = function(targetId,issueName,targetName){
+       console.log(targetName);
+       $scope.dataKpi = {};
+       $scope.dataStrategy = {};
+       $scope.fetchKpi(targetId);
+       $scope.fetchStrategy(targetId);
+       
+       $scope.issueLabel = issueName;
+       $scope.targetLabel = targetName;
+       
+       $scope.targetId = targetId;
+       $scope.pageLevel = 2;
+   };
+   
+   
+   
+    
+    
+    $scope.modal = function(action,type,editId,editName,arrayIndex,issueId) {
         $scope.action = action;
-        $scope.textName = name;
-        $scope.editId = editId;
+        $scope.type = type;
+        $scope.textName = editName;
+        $scope.idEdit = editId;
+        $scope.arrayIndex = arrayIndex;
+        $scope.issueId = issueId;
         
         $scope.centerModal();
         $("#modal").modal();
     };
-
-   
-    $scope.soitTextName = function(){
-        var text="ลำดับชั้นประเภทหมวดรายจ่าย : ";
-        var check = 0;
-        for(var i = 0; i < $scope.nameLevel.length; i += 1) {
-            if(i>0){
-                text += " > ";
-            }
-            text += $scope.nameLevel[i];
-            check = 1;
-        }
-        
-        if(!check){ text=""; }
-        $scope.nametextLevel = text;
+    
+    
+    
+    $scope.fetchType = function() {
+        $http.get("fetchType").success(function(response) {
+            //console.log(JSON.stringify(response.dataList, null, 4));
+            $scope.dataType = response.dataList;
+        });
     };
     
-    $scope.fetchBudgetType = function(masterId,action,name) {
-        $scope.dataBudgetType = 0;
-        
-        if(action==="start"){
-            $scope.idLevel = [masterId];
-            $scope.nameLevel  = [];
-        }if(action==="next"){
-            if(!$scope.findValueArray($scope.idLevel,masterId)){
-               $scope.idLevel.push(masterId); 
-               $scope.nameLevel.push(name);
-            }
-        }else if(action==="back"){
-            $scope.idLevel.splice($scope.idLevel.length-1, 1);
-            $scope.nameLevel.splice($scope.nameLevel.length-1,1);
+    $scope.checkHaveData = function(){
+        if($scope.dataIssueAndTarget === null){
             
         }
+    };
+    
+    $scope.fetchIssueAndTarget = function() {
+        $scope.dataIssueAndTarget = 0;
+        var sendData = {pData:{mainPlanTypeId:$scope.selectType,budgetYear:$scope.selectYear}};
         
-        $scope.masterId = masterId;
-        //console.log($scope.findValueArray($scope.idLevel,0));
-        var sendData = {pData:{budgetYear:$scope.selectYear,masterId:masterId,isActive:1}};
-        $http.post("fetchBudgetType",sendData).then(function(response) {
+        $http.post("fetchIssueAndTarget",sendData).then(function(response) {
+ 
+            $scope.dataIssueAndTarget = response.data.dataList;
+            
+        });
+
+    };
+    
+    $scope.fetchKpi = function(targetId) {
+        var sendData = {pData:{mainPlanTargetId:targetId}};
+        $http.post("fetchKpi",sendData).then(function(response) {
             //console.log(JSON.stringify(response.data.dataList, null, 4));
-            $scope.dataBudgetType = response.data.dataList;
-            $scope.soitTextName();
+            $scope.dataKpi = response.data.dataList;
+        });
+    };
+    
+    $scope.fetchStrategy = function(targetId) {
+        var sendData = {pData:{mainPlanTargetId:targetId}};
+        $http.post("fetchStrategy",sendData).then(function(response) {
+            //console.log(JSON.stringify(response.data.dataList, null, 4));
+            $scope.dataStrategy = response.data.dataList;
         });
     };
     
     
-    $scope.back = function(){
-        $scope.fetchBudgetType($scope.idLevel[$scope.idLevel.length-2],"back");
-    };
+    
+    
     
     $scope.saveData = function(){
-        console.log("test");
+        
         var pData = {};
-        pData.typeName = $scope.textName;
-        pData.id = $scope.editId;
-        pData.masterId = $scope.masterId;
-        pData.budgetYear = $scope.selectYear;
-        pData.isActive = 1;
+        
+        
+        pData.id = $scope.idEdit;
+        
+        if($scope.type==="kpi"){
+            pData.mainPlanTargetId = $scope.targetId;
+            pData.kpiName = $scope.textName;
+        }else if($scope.type==="strategy"){
+            pData.mainPlanTargetId = $scope.targetId;
+            pData.strategyName = $scope.textName;
+        }else if($scope.type==="target"){
+            pData.mainPlanIssueId = $scope.issueId;
+            pData.targetName = $scope.textName;
+        }else if($scope.type==="issue"){
+            pData.mainPlanTypeId = $scope.selectType;
+            pData.issueName = $scope.textName;
+        }
+        
+        if($scope.action==="add"){
+            pData.budgetYear = $scope.selectYear;
+        }
         
         var sendData = {pData:pData};
-        $http.post("saveBudgetType",sendData).then(function(response) {
-            if($scope.action==="add"){
-                $scope.dataBudgetType.push(response.data.dataList);
-            }else if($scope.action==="edit"){
-                $scope.dataBudgetType[$scope.findIndexObject($scope.dataBudgetType,"id",$scope.editId)].typeName=$scope.textName;
+        
+        
+        
+        //console.log(JSON.stringify(pData, null, 4));
+        $http.post("save"+$scope.firstLetterUpperCase($scope.type),sendData).then(function(response) {
+            
+            if($scope.type==="kpi"){
+                if($scope.action==="add"){
+                    $scope.dataKpi.push(response.data.dataList);
+                }else if($scope.action==="edit"){
+                    $scope.dataKpi[$scope.findIndexObject($scope.dataKpi,"id",$scope.idEdit)].kpiName=$scope.textName;
+                }
+            }else if($scope.type==="strategy"){
+                if($scope.action==="add"){
+                    $scope.dataStrategy.push(response.data.dataList);
+                }else if($scope.action==="edit"){
+                    $scope.dataStrategy[$scope.findIndexObject($scope.dataStrategy,"id",$scope.idEdit)].strategyName=$scope.textName;
+                }
+            }else if($scope.type==="target"){
+                var arrayTarget = {idTarget:response.data.dataList.id,nameTarget:response.data.dataList.targetName};
+                if($scope.action==="add"){
+                    //console.log(JSON.stringify(response.data.dataList.targetName, null, 4));
+                    if($scope.dataIssueAndTarget[$scope.arrayIndex].hasOwnProperty('target')){
+                        $scope.dataIssueAndTarget[$scope.arrayIndex].target.push(arrayTarget);  
+                        console.log($scope.dataIssueAndTarget[$scope.arrayIndex].target);
+                    }else{
+                        var arrayTarget = [{idTarget:response.data.dataList.id,nameTarget:response.data.dataList.targetName}];
+                        $scope.dataIssueAndTarget[$scope.arrayIndex].target = arrayTarget;
+                    }
+                    
+                }else if($scope.action==="edit"){
+                    $scope.dataIssueAndTarget[$scope.arrayIndex].target[$scope.findIndexObject($scope.dataIssueAndTarget[$scope.arrayIndex].target,"idTarget",$scope.idEdit)].nameTarget=$scope.textName;
+                }
+            }else if($scope.type==="issue"){
+                var arrayIssue = {idIssue:response.data.dataList.id,nameIssue:response.data.dataList.issueName};
+                if($scope.action==="add"){
+                    //console.log(JSON.stringify(arrayIssue, null, 4));
+                    //console.log(JSON.stringify($scope.dataIssue, null, 4));
+                    $scope.dataIssueAndTarget.push(arrayIssue);
+                    //console.log(JSON.stringify($scope.dataIssueAndTarget, null, 4));
+                }else if($scope.action==="edit"){
+                    $scope.dataIssueAndTarget[$scope.findIndexObject($scope.dataIssueAndTarget,"idIssue",$scope.idEdit)].nameIssue=$scope.textName;
+                }
             }
+            
             $("#modal").modal('hide');
         });
+        
     };
     
-    $scope.delData = function(delId){
+    
+    
+    
+    $scope.delData = function(type,delId,arrayIndex){
         
         if(confirm("ยืนยันการลบข้อมูล")){
-            var sendData = {pData:{id:delId,isActive:0}};
-            $http.post("delBudgetType",sendData).then(function(response) {
-                $scope.dataBudgetType.splice($scope.findIndexObject($scope.dataBudgetType,"id",response.data.dataList.id), 1);
+            var sendData = {pData:{id:delId}};
+            $http.post("del"+$scope.firstLetterUpperCase(type),sendData).then(function(response) {
+                if(type==="kpi"){
+                    $scope.dataKpi.splice($scope.findIndexObject($scope.dataKpi,"id",response.data.dataList.id), 1);
+                }else if(type==="strategy"){
+                    $scope.dataStrategy.splice($scope.findIndexObject($scope.dataStrategy,"id",response.data.dataList.id), 1);
+                }else if(type==="target"){
+                    $scope.dataIssueAndTarget[arrayIndex].target.splice($scope.findIndexObject($scope.dataIssueAndTarget[arrayIndex].target,"idTarget",response.data.dataList.id), 1);
+                }else if(type==="issue"){
+                    $scope.dataIssueAndTarget.splice($scope.findIndexObject($scope.dataIssueAndTarget,"idIssue",response.data.dataList.id), 1);
+                }
+                
             });
         }
     };
+    
+    
+    
+    
+    
+    
     
     
     
