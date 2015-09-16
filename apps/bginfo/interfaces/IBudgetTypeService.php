63a7fcedf199bb492c56e8ccc52e3ceaca80d2bf
Pongpanot Tassaneeyachumphalee <pongpanot.t@baanprogram.com>
2015-09-16 19:59:57 +0700

    Edit Bginfo and Report V2

---
+++ apps/bginfo/interfaces/IBudgetTypeService.php
@@ -0,0 +1,47 @@
+<?php
+namespace apps\bginfo\interfaces;
+
+/**
+ * @name BudgetTypeService
+ * @uri /BudgetType
+ * @description ระบบจัดการประเภทหมวดรายจ่าย
+ */
+interface IBudgetTypeService {
+    
+    /**
+     * @name manage
+     * @uri /manage
+     * @description หน้าจัดการประเภทหมวดรายจ่าย
+     */ 
+    public function viewManage();
+    
+    /**
+     * @name fetchBudgetType
+     * @uri /fetchBudgetType
+     * @param apps\common\entity\BudgetType pData Description
+     * @description ดึงข้อมูลประเภทหมวดรายจ่าย
+     * @return apps\common\entity\BudgetType dataList
+     */ 
+    public function fetchBudgetType($pData);
+    
+    
+    /**
+     * @name saveBudgetType
+     * @uri /saveBudgetType
+     * @param apps\common\entity\BudgetType pData Description
+     * @description บันทึกข้อมูลประเภทหมวดรายจ่าย
+     * @return apps\common\entity\BudgetType dataList
+     */ 
+    public function saveBudgetType($pData);
+    
+    /**
+     * @name delBudgetType
+     * @uri /delBudgetType
+     * @param apps\common\entity\BudgetType pData Description
+     * @description ลบข้อมูลประเภทหมวดรายจ่าย
+     * @return apps\common\entity\BudgetType dataList
+     */ 
+    public function delBudgetType($pData);
+    
+
+}
