bd89604a0cb7c6990f4c0bf5cd59b2e4d323c755
Panya Noipram <panya.n@baanprogram.com>
2015-09-26 16:25:52 +0700

    Department bginfo

--- apps/bginfo/interfaces/IDepartmentService.php
+++ apps/bginfo/interfaces/IDepartmentService.php
@@ -35,4 +35,55 @@ interface IDepartmentService
      */
     public function fetchDepartment($campusID);
 
+
+    /**
+     * @name fetchDepartmentMain
+     * @uri /fetchDepartmentMain
+     * @param String campusID Description
+     * @return String[] listsDepartment
+     * @description ข้อมูลหน่วยงานหลัก
+     */
+    public function fetchDepartmentMain($campusID);
+
+
+    /**
+     * @name fetchActivityType
+     * @uri /fetchActivityType
+     * @return String[] listsActivityType
+     * @description รายการประเภทส่วนงาน
+     */
+    public function fetchActivityType();
+
+
+    /**
+     * @name saveDept
+     * @uri /saveDept
+     * @param apps\common\entity\L3D\Department dataDept Description
+     * @param apps\common\entity\MappingDepartmentType dataMaping Description
+     * @return String mapid
+     * @description บันทึกหน่วยงาน
+     */
+    public function saveDepartment($dataDept, $dataMaping);
+
+    /**
+     * @name editDept
+     * @uri /editDept
+     * @param apps\common\entity\L3D\Department dataDept Description
+     * @param apps\common\entity\MappingDepartmentType dataMaping Description
+     * @return boolean status
+     * @description แก้ไขหน่วยงาน
+     */
+    public function editDepartment($dataDept, $dataMaping);
+
+
+    /**
+     * @name removeDept
+     * @uri /removeDept
+     * @param string idDept Description
+     * @param string mapid Description
+     * @return boolean status
+     * @description ลบหน่วยงาน
+     */
+    public function removeDepartment($idDept, $mapid);
+
 }
