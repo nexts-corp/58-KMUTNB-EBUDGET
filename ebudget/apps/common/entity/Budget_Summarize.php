4322798c04b749b147915c44f0b80804f732bd40
palida <palida.p@baanprogram.com>
2016-01-05 10:53:12 +0700

    add new entity Budget_Summarize

---
+++ ebudget/apps/common/entity/Budget_Summarize.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace apps\common\entity;
+
+/**
+ * @Entity
+ * @Table(name="Budget_Summarize")
+ */
+class Budget_Summarize extends EntityBase {
+
+    /**
+     * @Id 
+     * @Column(type="integer",length=11,name="Id")
+     * @GeneratedValue
+     */
+    public $id;
+
+    /** @Column(type="integer",length=11, name="BudgetPeriodId") */
+    public $bgPeriodId;
+
+    /** @Column(type="integer",length=11, name="BudgetTypeId") */
+    public $bgTypeId;
+
+    /** @Column(type="integer",length=11, name="DepartmentId") */
+    public $deptId;
+
+    /** @Column(type="string",length=1, name="BUDGETTYPECODE") */
+    public $bgTypeCode;
+
+    /** @Column(type="string",length=18, name="BudgetBeforeReview") */
+    public $bgBeforeReview;
+
+    /** @Column(type="string",length=18, name="BudgetAfterReview") */
+    public $bgAfterReview;
+
+    /** @Column(type="string",length=18, name="BudgetFinal") */
+    public $bgFinal;
+
+    /** @Column(type="string",length=20, name="CREATEUSERID") */
+    public $creator;
+
+    /** @Column(type="string",length=20, name="LASTUPDATEUSERID") */
+    public $updater;
+
+    function getId() {
+        return $this->id;
+    }
+
+    function getBgPeriodId() {
+        return $this->bgPeriodId;
+    }
+
+    function getBgTypeId() {
+        return $this->bgTypeId;
+    }
+
+    function getDeptId() {
+        return $this->deptId;
+    }
+
+    function getBgTypeCode() {
+        return $this->bgTypeCode;
+    }
+
+    function getBgBeforeReview() {
+        return $this->bgBeforeReview;
+    }
+
+    function getBgAfterReview() {
+        return $this->bgAfterReview;
+    }
+
+    function getBgFinal() {
+        return $this->bgFinal;
+    }
+
+    function getCreator() {
+        return $this->creator;
+    }
+
+    function getUpdater() {
+        return $this->updater;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setBgPeriodId($bgPeriodId) {
+        $this->bgPeriodId = $bgPeriodId;
+    }
+
+    function setBgTypeId($bgTypeId) {
+        $this->bgTypeId = $bgTypeId;
+    }
+
+    function setDeptId($deptId) {
+        $this->deptId = $deptId;
+    }
+
+    function setBgTypeCode($bgTypeCode) {
+        $this->bgTypeCode = $bgTypeCode;
+    }
+
+    function setBgBeforeReview($bgBeforeReview) {
+        $this->bgBeforeReview = $bgBeforeReview;
+    }
+
+    function setBgAfterReview($bgAfterReview) {
+        $this->bgAfterReview = $bgAfterReview;
+    }
+
+    function setBgFinal($bgFinal) {
+        $this->bgFinal = $bgFinal;
+    }
+
+    function setCreator($creator) {
+        $this->creator = $creator;
+    }
+
+    function setUpdater($updater) {
+        $this->updater = $updater;
+    }
+
+}
