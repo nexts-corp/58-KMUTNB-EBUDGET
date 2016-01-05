83524fdca2c942c80b7a5e768674d56ee00a1dc8
PIN <psingchai@gmail.com>
2016-01-05 15:16:03 +0700

    new EduPlan

--- ebudget/apps/bginfo/interfaces/IEduDevPlanService.php
+++ ebudget/apps/bginfo/interfaces/IEduDevPlanService.php
@@ -7,127 +7,6 @@ namespace apps\bginfo\interfaces;
  * @description ระบบจัดการด้านแผนพัฒนาการศึกษาระดับอุดมศึกษา
  */
 interface IEduDevPlanService {
-    
-    /**
-     * @name manage
-     * @uri /manage
-     * @description หน้าจัดการแผนงาน
-     */ 
-    public function viewManage();
-    
-    /**
-     * @name fetchType
-     * @uri /fetchType
-     * @description ดึงข้อมูลแผนกลยุทธ์
-     * @return apps\common\entity\MainPlanType dataList
-     */ 
-    public function fetchType();
-    
-    /**
-     * @name fetchIssueAndTarget
-     * @uri /fetchIssueAndTarget
-     * @param String[] pData Description
-     * @description ดึงข้อมูลประเด็นยุทธศาสตร์และเป้าประสงค์​
-     * @return String[] dataList
-     */ 
-    public function fetchIssueAndTarget($pData);
-    
-    /**
-     * @name fetchKpi
-     * @uri /fetchKpi
-     * @param apps\common\entity\AffirmativeKpi pData Description
-     * @description ดึงข้อมูลตัวชี้วัด
-     * @return apps\common\entity\AffirmativeKpi dataList
-     */ 
-    public function fetchKpi($pData);
-    
-    
-    /**
-     * @name fetchStrategy
-     * @uri /fetchStrategy
-     * @param apps\common\entity\AffirmativeStrategy pData Description
-     * @description ดึงข้อมูลกลยุทธ์
-     * @return apps\common\entity\AffirmativeStrategy dataList
-     */ 
-    public function fetchStrategy($pData);
-    
-    
-    
-    
-    /**
-     * @name saveKpi
-     * @uri /saveKpi
-     * @param apps\common\entity\AffirmativeKpi pData Description
-     * @description บันทึกข้อมูลตัวชี้วัด
-     * @return apps\common\entity\AffirmativeKpi dataList
-     */ 
-    public function saveKpi($pData);
-    
-    /**
-     * @name saveStrategy
-     * @uri /saveStrategy
-     * @param apps\common\entity\AffirmativeStrategy pData Description
-     * @description บันทึกข้อมูลกลยุทธ์
-     * @return apps\common\entity\AffirmativeStrategy dataList
-     */ 
-    public function saveStrategy($pData);
-    
-    /**
-     * @name saveTarget
-     * @uri /saveTarget
-     * @param apps\common\entity\AffirmativeTarget pData Description
-     * @description บันทึกข้อมูลเป้าประสงค์
-     * @return apps\common\entity\AffirmativeTarget dataList
-     */ 
-    public function saveTarget($pData);
-    
-    /**
-     * @name saveIssue
-     * @uri /saveIssue
-     * @param apps\common\entity\AffirmativeIssue pData Description
-     * @description บันทึกข้อมูลประเด็นยุทธศาสตร์
-     * @return apps\common\entity\AffirmativeIssue dataList
-     */ 
-    public function saveIssue($pData);
-    
-    
-    /**
-     * @name delKpi
-     * @uri /delKpi
-     * @param apps\common\entity\AffirmativeKpi pData Description
-     * @description ลบข้อมูลตัวชี้วัด
-     * @return apps\common\entity\AffirmativeKpi dataList
-     */ 
-    public function delKpi($pData);
-    
-    /**
-     * @name delStrategy
-     * @uri /delStrategy
-     * @param apps\common\entity\AffirmativeStrategy pData Description
-     * @description ลบข้อมูลกลยุทธ
-     * @return apps\common\entity\AffirmativeStrategy dataList
-     */ 
-    public function delStrategy($pData);
-    
-    /**
-     * @name delTarget
-     * @uri /delTarget
-     * @param apps\common\entity\AffirmativeTarget pData Description
-     * @description ลบข้อมูลเป้าประสงค์
-     * @return apps\common\entity\AffirmativeTarget dataList
-     */ 
-    public function delTarget($pData);
-    
-    /**
-     * @name delIssue
-     * @uri /delIssue
-     * @param apps\common\entity\AffirmativeIssue pData Description
-     * @description ลบข้อมูลประเด็นยุทธศาสตร์
-     * @return apps\common\entity\AffirmativeIssue dataList
-     */ 
-    public function delIssue($pData);
-
-
     /**
      * @name listsType
      * @uri /listsType
@@ -253,4 +132,118 @@ interface IEduDevPlanService {
      */
     public function deleteStrategy($id);
 
+    /**
+     * @name manage
+     * @uri /manage
+     * @description หน้าจัดการแผนงาน
+     */ 
+    //public function viewManage();
+    
+    /**
+     * @name fetchType
+     * @uri /fetchType
+     * @description ดึงข้อมูลแผนกลยุทธ์
+     * @return apps\common\entity\MainPlanType dataList
+     */ 
+    //public function fetchType();
+    
+    /**
+     * @name fetchIssueAndTarget
+     * @uri /fetchIssueAndTarget
+     * @param String[] pData Description
+     * @description ดึงข้อมูลประเด็นยุทธศาสตร์และเป้าประสงค์​
+     * @return String[] dataList
+     */ 
+    //public function fetchIssueAndTarget($pData);
+    
+    /**
+     * @name fetchKpi
+     * @uri /fetchKpi
+     * @param apps\common\entity\AffirmativeKpi pData Description
+     * @description ดึงข้อมูลตัวชี้วัด
+     * @return apps\common\entity\AffirmativeKpi dataList
+     */ 
+    //public function fetchKpi($pData);
+
+    /**
+     * @name fetchStrategy
+     * @uri /fetchStrategy
+     * @param apps\common\entity\AffirmativeStrategy pData Description
+     * @description ดึงข้อมูลกลยุทธ์
+     * @return apps\common\entity\AffirmativeStrategy dataList
+     */ 
+    //public function fetchStrategy($pData);
+
+    /**
+     * @name saveKpi
+     * @uri /saveKpi
+     * @param apps\common\entity\AffirmativeKpi pData Description
+     * @description บันทึกข้อมูลตัวชี้วัด
+     * @return apps\common\entity\AffirmativeKpi dataList
+     */ 
+    //public function saveKpi($pData);
+    
+    /**
+     * @name saveStrategy
+     * @uri /saveStrategy
+     * @param apps\common\entity\AffirmativeStrategy pData Description
+     * @description บันทึกข้อมูลกลยุทธ์
+     * @return apps\common\entity\AffirmativeStrategy dataList
+     */ 
+    //public function saveStrategy($pData);
+    
+    /**
+     * @name saveTarget
+     * @uri /saveTarget
+     * @param apps\common\entity\AffirmativeTarget pData Description
+     * @description บันทึกข้อมูลเป้าประสงค์
+     * @return apps\common\entity\AffirmativeTarget dataList
+     */ 
+   //public function saveTarget($pData);
+    
+    /**
+     * @name saveIssue
+     * @uri /saveIssue
+     * @param apps\common\entity\AffirmativeIssue pData Description
+     * @description บันทึกข้อมูลประเด็นยุทธศาสตร์
+     * @return apps\common\entity\AffirmativeIssue dataList
+     */ 
+    //public function saveIssue($pData);
+    
+    
+    /**
+     * @name delKpi
+     * @uri /delKpi
+     * @param apps\common\entity\AffirmativeKpi pData Description
+     * @description ลบข้อมูลตัวชี้วัด
+     * @return apps\common\entity\AffirmativeKpi dataList
+     */ 
+    //public function delKpi($pData);
+    
+    /**
+     * @name delStrategy
+     * @uri /delStrategy
+     * @param apps\common\entity\AffirmativeStrategy pData Description
+     * @description ลบข้อมูลกลยุทธ
+     * @return apps\common\entity\AffirmativeStrategy dataList
+     */ 
+    //public function delStrategy($pData);
+    
+    /**
+     * @name delTarget
+     * @uri /delTarget
+     * @param apps\common\entity\AffirmativeTarget pData Description
+     * @description ลบข้อมูลเป้าประสงค์
+     * @return apps\common\entity\AffirmativeTarget dataList
+     */ 
+    //public function delTarget($pData);
+    
+    /**
+     * @name delIssue
+     * @uri /delIssue
+     * @param apps\common\entity\AffirmativeIssue pData Description
+     * @description ลบข้อมูลประเด็นยุทธศาสตร์
+     * @return apps\common\entity\AffirmativeIssue dataList
+     */ 
+    //public function delIssue($pData);
 }
--- ebudget/apps/bginfo/service/EduDevPlanService.php
+++ ebudget/apps/bginfo/service/EduDevPlanService.php
@@ -26,151 +26,10 @@ class EduDevPlanService extends CServiceBase implements IEduDevPlanService {
     function getPeriod() {
         $year = new \apps\common\entity\Year();
         $year->yearStatus = 'Y';
-        return $this->datacontext->getObject($year)[0];
-        //$data = $this->datacontext->getObject($year)[0];
-        //$data->year = 2559;
-        //return $data;
-    }
-
-    public function viewManage() {
-        $view = new CJView("EduDevPlan/manage", CJViewType::HTML_VIEW_ENGINE);
-        return $view;
-        
-    }
-    
-    public function fetchType() {
-        $list = new AffirmativeType();
-        return $this->datacontext->getObject($list);
-    }
-    
-    public function fetchIssueAndTarget($pData) {
-        
-  
-        $sql = "
-            SELECT nIssue.id AS idIssue , nIssue.issueName AS nameIssue , nTarget.id AS idTarget , nTarget.targetName AS nameTarget
-            FROM ".$this->pathEnt."\\AffirmativeIssue nIssue
-            LEFT JOIN ".$this->pathEnt."\\AffirmativeTarget nTarget
-            WITH nIssue.id = nTarget.issueId
-            WHERE nIssue.typeId = :typeId
-            ORDER BY nIssue.id,nTarget.id
-        ";
-            
-        $dataIAT = $this->datacontext->getObject($sql,array("typeId"=>$pData->typeId));
-        
-    
-        $dataList = null;
-        $idIssueOld = null;
-        $idIssueNew = null;
-        
-        $j = 0;
-        $k = 0;
-        for ($i = 0; $i < count($dataIAT); $i++) {
-            
-            
-            $idIssueNew = $dataIAT[$i]["idIssue"];
-            
-            if($idIssueOld!=$idIssueNew){
-                
-                $dataList[$j]["idIssue"] = $dataIAT[$i]["idIssue"];
-                $dataList[$j]["nameIssue"] = $dataIAT[$i]["nameIssue"];
-                
-                $j++;
-                $k = 0;
-                $idIssueOld = $idIssueNew;
-            }
-            
-            if($dataIAT[$i]["idTarget"]!=""){
-                $dataList[$j-1]["target"][$k]["idTarget"] = $dataIAT[$i]["idTarget"];
-                $dataList[$j-1]["target"][$k]["nameTarget"] = $dataIAT[$i]["nameTarget"];
-            }
-            $k++;
-
-        }
-        
-        return $dataList;
-        
-    }
-    
-    
-    public function fetchKpi($pData) {
-        
-        $list = new AffirmativeKpi;
-        $list->setTargetId($pData->targetId);
-        return $this->datacontext->getObject($list);
-        //return $pData;
-        
-    }
-    
-    public function fetchStrategy($pData) {
-        
-        $list = new AffirmativeStrategy();
-        $list->setTargetId($pData->targetId);
-        return $this->datacontext->getObject($list);
-        
-    }
-    
-    
-    
-    
-    public function saveKpi($pData) {
-        
-        if($pData->id==null){ 
-            $this->datacontext->saveObject($pData);
-        }else{
-            $this->datacontext->updateObject($pData);
-        }
-        return $pData;
-        
-    }
-    
-    public function saveStrategy($pData) {
-        
-        if($pData->id==null){ 
-            $this->datacontext->saveObject($pData);
-        }else{
-            $this->datacontext->updateObject($pData);
-        }
-        return $pData;
-        
-    }
-    
-    public function saveTarget($pData) {
-        
-        if($pData->id==null){ 
-            $this->datacontext->saveObject($pData);
-        }else{
-            $this->datacontext->updateObject($pData);
-        }
-        return $pData;
-        
-    }
-    
-    public function saveIssue($pData) {
-        
-        if($pData->id==null){ 
-            $this->datacontext->saveObject($pData);
-        }else{
-            $this->datacontext->updateObject($pData);
-        }
-        return $pData;
-        
-    }
-
-    public function delKpi($pData) {
-        $this->datacontext->removeObject($pData);
-        return $pData;
-    }
-    public function delStrategy($pData) {
-        $this->datacontext->removeObject($pData);
-        return $pData;
-    }
-    public function delTarget($pData) {
-        $this->datacontext->removeObject($pData);
-        return $pData;
-    }
-    public function delIssue($pData) {
-        $this->datacontext->removeObject($pData);
-        return $pData;
+        //return $this->datacontext->getObject($year)[0];
+        $data = $this->datacontext->getObject($year)[0];
+        $data->year = 2559;
+        return $data;
     }
 
     public function listsType() {
@@ -461,4 +320,139 @@ class EduDevPlanService extends CServiceBase implements IEduDevPlanService {
         }
         return $return;
     }
+
+    /*
+    public function viewManage() {
+        $view = new CJView("EduDevPlan/manage", CJViewType::HTML_VIEW_ENGINE);
+        return $view;
+        
+    }
+    
+    public function fetchType() {
+        $list = new AffirmativeType();
+        return $this->datacontext->getObject($list);
+    }
+    
+    public function fetchIssueAndTarget($pData) {
+        
+  
+        $sql = "
+            SELECT nIssue.id AS idIssue , nIssue.issueName AS nameIssue , nTarget.id AS idTarget , nTarget.targetName AS nameTarget
+            FROM ".$this->pathEnt."\\AffirmativeIssue nIssue
+            LEFT JOIN ".$this->pathEnt."\\AffirmativeTarget nTarget
+            WITH nIssue.id = nTarget.issueId
+            WHERE nIssue.typeId = :typeId
+            ORDER BY nIssue.id,nTarget.id
+        ";
+            
+        $dataIAT = $this->datacontext->getObject($sql,array("typeId"=>$pData->typeId));
+        
+    
+        $dataList = null;
+        $idIssueOld = null;
+        $idIssueNew = null;
+        
+        $j = 0;
+        $k = 0;
+        for ($i = 0; $i < count($dataIAT); $i++) {
+            
+            
+            $idIssueNew = $dataIAT[$i]["idIssue"];
+            
+            if($idIssueOld!=$idIssueNew){
+                
+                $dataList[$j]["idIssue"] = $dataIAT[$i]["idIssue"];
+                $dataList[$j]["nameIssue"] = $dataIAT[$i]["nameIssue"];
+                
+                $j++;
+                $k = 0;
+                $idIssueOld = $idIssueNew;
+            }
+            
+            if($dataIAT[$i]["idTarget"]!=""){
+                $dataList[$j-1]["target"][$k]["idTarget"] = $dataIAT[$i]["idTarget"];
+                $dataList[$j-1]["target"][$k]["nameTarget"] = $dataIAT[$i]["nameTarget"];
+            }
+            $k++;
+        }
+        
+        return $dataList;
+    }
+    
+    
+    public function fetchKpi($pData) {
+        
+        $list = new AffirmativeKpi;
+        $list->setTargetId($pData->targetId);
+        return $this->datacontext->getObject($list);
+        //return $pData;
+    }
+    
+    public function fetchStrategy($pData) {
+        
+        $list = new AffirmativeStrategy();
+        $list->setTargetId($pData->targetId);
+        return $this->datacontext->getObject($list);
+    }
+
+    public function saveKpi($pData) {
+
+        if($pData->id==null){ 
+            $this->datacontext->saveObject($pData);
+        }else{
+            $this->datacontext->updateObject($pData);
+        }
+        return $pData;
+    }
+    
+    public function saveStrategy($pData) {
+        
+        if($pData->id==null){ 
+            $this->datacontext->saveObject($pData);
+        }else{
+            $this->datacontext->updateObject($pData);
+        }
+        return $pData;
+    }
+    
+    public function saveTarget($pData) {
+        
+        if($pData->id==null){ 
+            $this->datacontext->saveObject($pData);
+        }else{
+            $this->datacontext->updateObject($pData);
+        }
+        return $pData;
+    }
+    
+    public function saveIssue($pData) {
+        
+        if($pData->id==null){ 
+            $this->datacontext->saveObject($pData);
+        }else{
+            $this->datacontext->updateObject($pData);
+        }
+        return $pData;
+    }
+
+    public function delKpi($pData) {
+        $this->datacontext->removeObject($pData);
+        return $pData;
+    }
+
+    public function delStrategy($pData) {
+        $this->datacontext->removeObject($pData);
+        return $pData;
+    }
+
+    public function delTarget($pData) {
+        $this->datacontext->removeObject($pData);
+        return $pData;
+    }
+
+    public function delIssue($pData) {
+        $this->datacontext->removeObject($pData);
+        return $pData;
+    }
+    */
 }
