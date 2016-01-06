ab4bcd974ba01057ce440fd60ea58b04c0888e59
tawatchai.w <tawatchai.w@tawatchaiw-PC.nexts-corp.com>
2016-01-06 10:18:49 +0700

    success final142

--- ebudget/apps/budget/service/Final142Service.php
+++ ebudget/apps/budget/service/Final142Service.php
@@ -11,7 +11,6 @@ use th\co\bpg\cde\data\CDataContext;
 use apps\budget\interfaces\IFinal142Service;
 use apps\common\entity;
 use apps\common\entity\BudgetHead;
-
 use th\co\bpg\cde\collection\impl\CJSONDecodeImpl;
 
 class Final142Service extends CServiceBase implements IFinal142Service {
@@ -29,8 +28,7 @@ class Final142Service extends CServiceBase implements IFinal142Service {
         return $this->datacontext->getObject($year)[0];
     }
 
-    function getBudgetPlanAndProject($budgetPeriodId, $L3DPlanId, $fundgroupId)
-    {
+    function getBudgetPlanAndProject($budgetPeriodId, $L3DPlanId, $fundgroupId) {
         $project = new \apps\common\entity\MappingPlan();
 
         $project->setBudgetperiodId($budgetPeriodId);
@@ -60,19 +58,19 @@ class Final142Service extends CServiceBase implements IFinal142Service {
         return $result;
     }
 
-    public function view($param){
+    public function view($param) {
         $param->budgetPeriodId = $this->getPeriod()->year;
         $param->budgetTypeCode = "G";
 
         $sql1 = " SELECT typ.id, typ.typeName, typ.masterId "
-            . " FROM " . $this->ent . "\\BudgetType typ "
-            . " WHERE typ.masterId = '20100000' and typ.typeCode = 'G' and typ.form142 = true ";
+                . " FROM " . $this->ent . "\\BudgetType typ "
+                . " WHERE typ.masterId = '20100000' and typ.typeCode = 'G' and typ.form142 = true ";
         $list1 = $this->datacontext->getObject($sql1);
 
         foreach ($list1 as $key1 => $value1) {
             $sql2 = " SELECT typ.id, typ.typeName, typ.masterId "
-                . " FROM " . $this->ent . "\\BudgetType typ "
-                . " WHERE typ.masterId = :masterId ";
+                    . " FROM " . $this->ent . "\\BudgetType typ "
+                    . " WHERE typ.masterId = :masterId ";
             $param2 = array(
                 "masterId" => $list1[$key1]["id"]
             );
@@ -81,17 +79,18 @@ class Final142Service extends CServiceBase implements IFinal142Service {
 
             foreach ($list2 as $key2 => $value2) {
                 $sql3 = " select head.id AS budgetHeadId, bg.id, bg.positionName, bg.occupy, bg.vacancy, bg.rateNo, bg.salary, bg.salaryTotal, bg.remark,bg.comment,bg.attachmentId,att.desc,att.path,ts.id AS statusId,ts.desc AS statusDesc"
-                    . " from " . $this->ent . "\\Budget142 bg "
-                    . " left join " . $this->ent . "\\BudgetHead head with head.id = bg.budgetHeadId "
-                    . " left join " . $this->ent . "\\Attachment att with bg.attachmentId = att.id "
-                    . " left join " . $this->ent . "\\TrackingStatus ts with bg.statusId = ts.id "
-                    . " where head.formId = :formId "
-                    . " and bg.budgetTypeId = :budgetTypeId "
-                    . " and bg.budgetPeriodId = :budgetPeriodId "
-                    . " and bg.budgetTypeCode = :budgetTypeCode "
-                    . " and bg.l3dPlanId = :l3dPlanId "
-                    . " and bg.fundgroupId = :fundgroupId "
-                    . " and bg.deptId = :deptId";
+                        . " ,bg.status "
+                        . " from " . $this->ent . "\\Final142 bg "
+                        . " left join " . $this->ent . "\\BudgetHead head with head.id = bg.budgetHeadId "
+                        . " left join " . $this->ent . "\\Attachment att with bg.attachmentId = att.id "
+                        . " left join " . $this->ent . "\\TrackingStatus ts with bg.statusId = ts.id "
+                        . " where head.formId = :formId "
+                        . " and bg.budgetTypeId = :budgetTypeId "
+                        . " and bg.budgetPeriodId = :budgetPeriodId "
+                        . " and bg.budgetTypeCode = :budgetTypeCode "
+                        . " and bg.l3dPlanId = :l3dPlanId "
+                        . " and bg.fundgroupId = :fundgroupId "
+                        . " and bg.deptId = :deptId";
 
                 $param3 = array(
                     "formId" => "142",
@@ -141,14 +140,14 @@ class Final142Service extends CServiceBase implements IFinal142Service {
         return $list1;
     }
 
-    public function insert($budget, $file){
+    public function insert($budget, $file) {
 
         $conv = json_decode($budget);
 
         $desc = $conv->desc;
 
         $json = new CJSONDecodeImpl();
-        $budget = $json->decode(new \apps\common\entity\Budget142(),$conv);
+        $budget = $json->decode(new \apps\common\entity\Budget142(), $conv);
         $budget->budgetTypeCode = "G";
         $budget->budgetPeriodId = $this->getPeriod()->year;
 
@@ -198,13 +197,13 @@ class Final142Service extends CServiceBase implements IFinal142Service {
             $return["budgetHeadId"] = $bgHeadId;
         }
 
-        if($file != ''){
-            if($file != "undefined") {
+        if ($file != '') {
+            if ($file != "undefined") {
                 $time = date("YmdHis");
                 $target_dir = "apps\\budget\\views\\draft\\attachment\\";
 
-                $target_file = $target_dir ."BG142". $time . "-" . $file["name"];
-                $fileN = "BG142". $time . "-" . $file["name"];
+                $target_file = $target_dir . "BG142" . $time . "-" . $file["name"];
+                $fileN = "BG142" . $time . "-" . $file["name"];
 
                 if (move_uploaded_file($file["tmp_name"], $target_file)) {
 
@@ -214,8 +213,7 @@ class Final142Service extends CServiceBase implements IFinal142Service {
 
                     if (!$this->datacontext->saveObject($update)) {
                         $return = $this->datacontext->getLastMessage();
-                    }
-                    else{
+                    } else {
                         $update2 = new \apps\common\entity\Budget142();
                         $update2->id = $budget->id;
                         $data = $this->datacontext->getObject($update2);
@@ -225,14 +223,13 @@ class Final142Service extends CServiceBase implements IFinal142Service {
 
                         $return["path"] = $fileN;
                     }
-
                 }
             }
         }
         return $return;
     }
 
-    public function update($budget, $file, $fileUpload){
+    public function update($budget, $file, $fileUpload) {
         $return = array();
 
         $conv = json_decode($budget);
@@ -240,7 +237,7 @@ class Final142Service extends CServiceBase implements IFinal142Service {
         $desc = $conv->desc;
 
         $json = new CJSONDecodeImpl();
-        $budget = $json->decode(new \apps\common\entity\Budget142(),$conv);
+        $budget = $json->decode(new \apps\common\entity\Budget142(), $conv);
 
         $budget->bgSummary = $budget->salaryTotal;
         $budget->dateUpdated = date('Y-m-d H:i:s');
@@ -248,12 +245,11 @@ class Final142Service extends CServiceBase implements IFinal142Service {
         if (!$this->datacontext->updateObject($budget)) {
             $return["result"] = false;
             $return["msg"] = $this->datacontext->getLastMessage();
-        }
-        else {
+        } else {
             $return["result"] = true;
         }
 
-        if($fileUpload == "1"){
+        if ($fileUpload == "1") {
             $time = date("YmdHis");
             $target_dir = "apps\\budget\\views\\draft\\attachment\\";
 
@@ -274,21 +270,20 @@ class Final142Service extends CServiceBase implements IFinal142Service {
 
                     if (!$this->datacontext->updateObject($data[0])) {
                         $return = $this->datacontext->getLastMessage();
-                    }
-                    else{
-                        if (!$this->datacontext->removeObject($data2[0])){
+                    } else {
+                        if (!$this->datacontext->removeObject($data2[0])) {
                             $return = $this->datacontext->getLastMessage();
                         }
                     }
                 }
             }
 
-            if($file !== "undefined") {
+            if ($file !== "undefined") {
                 $time = date("YmdHis");
                 $target_dir = "apps\\budget\\views\\draft\\attachment\\";
 
-                $target_file = $target_dir ."BG142". $time . "-" . $file["name"];
-                $fileN = "BG142". $time . "-" . $file["name"];
+                $target_file = $target_dir . "BG142" . $time . "-" . $file["name"];
+                $fileN = "BG142" . $time . "-" . $file["name"];
 
                 if (move_uploaded_file($file["tmp_name"], $target_file)) {
 
@@ -298,8 +293,7 @@ class Final142Service extends CServiceBase implements IFinal142Service {
 
                     if (!$this->datacontext->saveObject($update)) {
                         $return = $this->datacontext->getLastMessage();
-                    }
-                    else{
+                    } else {
                         $update2 = new \apps\common\entity\Budget142();
                         $update2->id = $budget->id;
                         $data = $this->datacontext->getObject($update2);
@@ -309,14 +303,13 @@ class Final142Service extends CServiceBase implements IFinal142Service {
 
                         $return["path"] = $fileN;
                     }
-
                 }
             }
         }
         return $return;
     }
 
-    public function delete($budgetId){
+    public function delete($budgetId) {
         $result = true;
 
         $repo = new \apps\common\entity\Budget142();
@@ -340,11 +333,10 @@ class Final142Service extends CServiceBase implements IFinal142Service {
                     $result = false;
                     $return = $this->datacontext->getLastMessage();
                 }
-
             }
         }
 
-        if($attachmentId != null && $attachmentId != ""){
+        if ($attachmentId != null && $attachmentId != "") {
             $target_dir = "apps\\budget\\views\\draft\\attachment\\";
 
             $update2 = new \apps\common\entity\Attachment();
@@ -356,7 +348,7 @@ class Final142Service extends CServiceBase implements IFinal142Service {
 
                 $data[0]->attachmentId = null;
 
-                if (!$this->datacontext->removeObject($data2[0])){
+                if (!$this->datacontext->removeObject($data2[0])) {
                     $return = $this->datacontext->getLastMessage();
                 }
             }
@@ -364,4 +356,5 @@ class Final142Service extends CServiceBase implements IFinal142Service {
 
         return $result;
     }
+
 }
