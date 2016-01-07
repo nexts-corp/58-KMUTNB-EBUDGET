8562e327d19fdf5ab1480c1fef62684fa4a73a8f
Panya Noipram <panya.n@baanprogram.com>
2016-01-07 13:21:10 +0700

    approveAll

--- ebudget/apps/budget/service/ApproveSumService.php
+++ ebudget/apps/budget/service/ApproveSumService.php
@@ -31,27 +31,67 @@ class ApproveSumService extends CServiceBase implements IApproveSumService
         $this->datacontext = new CDataContext();
     }
 
-    public function approveSumBudgetRequest($year)
+    public function approveSumBudgetRequest($budgetPeriodId)
     {
 
-        $sql = " SELECT bgAll.*,bgType.BUDGETTYPENAME as bgTypeName FROM " .
-            "(" .
-            "SELECT formType,typeId,SUM(bgSum) as Summary FROM " .
-            "(" .
-            "SELECT '140' as formType,BUDGETHEADID as headId,BUDGETTYPEID as TypeId,BUDGETSUMMARY as bgSum FROM BUDGET140 WHERE BUDGETPERIODID = " . $year . " AND TRACKINGSTATUSID = 3 UNION ALL " .
-            "SELECT '141' as formType,BUDGETHEADID as headId,BUDGETTYPEID as TypeId,BUDGETSUMMARY as bgSum FROM BUDGET141 WHERE BUDGETPERIODID = " . $year . " AND TRACKINGSTATUSID = 3  UNION ALL " .
-            "SELECT '142' as formType,BUDGETHEADID as headId,BUDGETTYPEID as TypeId,BUDGETSUMMARY as bgSum FROM BUDGET142 WHERE BUDGETPERIODID = " . $year . " AND TRACKINGSTATUSID = 3  UNION ALL " .
-            "SELECT '143' as formType,BUDGETHEADID as headId,BUDGETTYPEID as TypeId,BUDGETSUMMARY as bgSum FROM BUDGET143 WHERE BUDGETPERIODID = " . $year . " AND TRACKINGSTATUSID = 3  UNION ALL " .
-            "SELECT '144' as formType,BUDGETHEADID as headId,BUDGETTYPEID as TypeId,BUDGETSUMMARY as bgSum FROM BUDGET144 WHERE BUDGETPERIODID = " . $year . " AND TRACKINGSTATUSID = 3  UNION ALL " .
-            "SELECT '145' as formType,BUDGETHEADID as headId,BUDGETTYPEID as TypeId,BUDGETSUMMARY as bgSum FROM BUDGET145 WHERE BUDGETPERIODID = " . $year . " AND TRACKINGSTATUSID = 3  UNION ALL " .
-            "SELECT '146' as formType,BUDGETHEADID as headId,BUDGETTYPEID as TypeId,BUDGETSUMMARY as bgSum FROM BUDGET146 WHERE BUDGETPERIODID = " . $year . " AND TRACKINGSTATUSID = 3" .
-            ") as BUDGETALL GROUP BY TypeId,formType " .
+//        $sql = " SELECT bgAll.*,bgType.BUDGETTYPENAME as bgTypeName FROM " .
+//            "(" .
+//            "SELECT formType,typeId,SUM(bgSum) as Summary FROM " .
+//            "(" .
+//            "SELECT '140' as formType,BUDGETHEADID as headId,BUDGETTYPEID as TypeId,BUDGETSUMMARY as bgSum FROM BUDGET140 WHERE BUDGETPERIODID = " . $year . " AND TRACKINGSTATUSID = 3 UNION ALL " .
+//            "SELECT '141' as formType,BUDGETHEADID as headId,BUDGETTYPEID as TypeId,BUDGETSUMMARY as bgSum FROM BUDGET141 WHERE BUDGETPERIODID = " . $year . " AND TRACKINGSTATUSID = 3  UNION ALL " .
+//            "SELECT '142' as formType,BUDGETHEADID as headId,BUDGETTYPEID as TypeId,BUDGETSUMMARY as bgSum FROM BUDGET142 WHERE BUDGETPERIODID = " . $year . " AND TRACKINGSTATUSID = 3  UNION ALL " .
+//            "SELECT '143' as formType,BUDGETHEADID as headId,BUDGETTYPEID as TypeId,BUDGETSUMMARY as bgSum FROM BUDGET143 WHERE BUDGETPERIODID = " . $year . " AND TRACKINGSTATUSID = 3  UNION ALL " .
+//            "SELECT '144' as formType,BUDGETHEADID as headId,BUDGETTYPEID as TypeId,BUDGETSUMMARY as bgSum FROM BUDGET144 WHERE BUDGETPERIODID = " . $year . " AND TRACKINGSTATUSID = 3  UNION ALL " .
+//            "SELECT '145' as formType,BUDGETHEADID as headId,BUDGETTYPEID as TypeId,BUDGETSUMMARY as bgSum FROM BUDGET145 WHERE BUDGETPERIODID = " . $year . " AND TRACKINGSTATUSID = 3  UNION ALL " .
+//            "SELECT '146' as formType,BUDGETHEADID as headId,BUDGETTYPEID as TypeId,BUDGETSUMMARY as bgSum FROM BUDGET146 WHERE BUDGETPERIODID = " . $year . " AND TRACKINGSTATUSID = 3" .
+//            ") as BUDGETALL GROUP BY TypeId,formType " .
+//
+//            ") bgAll INNER JOIN BUDGETTYPE bgType ON bgAll.typeId = bgType.BUDGETTYPEID ORDER BY bgAll.TypeId ";
 
-            ") bgAll INNER JOIN BUDGETTYPE bgType ON bgAll.typeId = bgType.BUDGETTYPEID ORDER BY bgAll.TypeId ";
 
-        $result = $this->datacontext->pdoQuery($sql);
+        $sql = 'exec SP_SUM_BG ' . $budgetPeriodId . ',"G"';
+        $status = $this->datacontext->pdoInsert($sql);
 
-        return $result;
+        if ($status != -1 && $status != false) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+
+    public function toFinalBg($year)
+    {
+        $sqlDelete = '';//140 - 146
+        $sqlInsert = '';
+        $sqlUpdate = '';
+        for ($i = 0; $i <= 6; $i++) {
+
+            $sqlDelete .= "DELETE  FROM Final_14" . $i . " WHERE BudgetPeriodId =" . $year . "; ";// clear data on table FINAL
+            $sqlInsert .= "INSERT INTO Final_14" . $i . " SELECT * FROM Budget_14" . $i . " WHERE BudgetPeriodId = " . $year . "; "; //Insert
+            $sqlUpdate .= "UPDATE Final_14" . $i . " SET Status = 'Y' WHERE BudgetPeriodId = " . $year . "; "; //Update status =y
+        }
+
+        $status = $this->datacontext->pdoDelete($sqlDelete);
+        if ($status != -1 && $status != false) {
+            $status = $this->datacontext->pdoInsert($sqlInsert);
+            if (!$status) {
+                $status = $this->datacontext->pdoUpdate($sqlUpdate);
+                if ($status != -1 && $status != false) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } else {
+
+            return false;
+        }
+
     }
 
     public function viewApproveSum($year) {
