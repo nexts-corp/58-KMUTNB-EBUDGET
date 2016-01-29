adca709cdd72ab17286b2a70577223762705d7e9
PIN <psingchai@gmail.com>
2016-01-29 16:47:53 +0700

    revenue service

--- ebudget/apps/revenue/interfaces/IPlaningService.php
+++ ebudget/apps/revenue/interfaces/IPlaningService.php
@@ -4,10 +4,18 @@ namespace apps\revenue\interfaces;
 
 /**
  * @name IPlaningService
- * @uri /planing
+ * @uri /planning
  * @description ประมูล
  */
 interface IPlaningService {
+    /**
+     * @name listsDepartment
+     * @uri /listsDepartment
+     * @return String[] lists Description
+     * @description รายชื่อหน่วยงาน
+     */
+    public function listsDepartment();
+
     /**
      * @name fetchRevenue
      * @uri /fetchRevenue
@@ -17,6 +25,24 @@ interface IPlaningService {
     public function fetchRevenue();
 
     /**
+     * @name addRevenue
+     * @uri /addRevenue
+     * @param apps\common\entity\BudgetRevenuePlan revenuePlan Description
+     * @return boolean add Description
+     * @description เพิ่มเงินจัดสรรเงินรายได้
+     */
+    public function addRevenue($revenuePlan);
+
+    /**
+     * @name updateRevenue
+     * @uri /updateRevenue
+     * @param apps\common\entity\BudgetRevenuePlan revenuePlan Description
+     * @return boolean update Description
+     * @description อัพเดทเงินจัดสรรเงินรายได้
+     */
+    public function updateRevenue($revenuePlan);
+
+    /**
      * @name deleteRevenue
      * @uri /deleteRevenue
      * @param int id
@@ -24,4 +50,45 @@ interface IPlaningService {
      * @description ลบเงินจัดสรรเงินรายได้
      */
     public function deleteRevenue($id);
+
+    /**
+     * @name fetchProject
+     * @uri /fetchProject
+     * @return string[] lists Description
+     * @description หน้าเพิ่มเงินจัดสรรเงินรายได้
+     */
+    public function fetchProject();
+
+    /**
+     * @name addProject
+     * @uri /addProject
+     * @param string projectName ชื่อโครงการ
+     * @param float[] budgetTotal
+     * @param int[] deptId
+     * @return boolean result Description
+     * @description หน้าเพิ่มเงินจัดสรรสำหรับโครงการพัฒนามหาวิทยาลัย
+     */
+    public function addProject($projectName, $budgetTotal, $deptId);
+
+    /**
+     * @name updateProject
+     * @uri /updateProject
+     * @param int bgHeadId
+     * @param string projectName ชื่อโครงการ
+     * @param int budgetPeriodId ปีงบประมาณ
+     * @param float[] budgetTotal
+     * @param int[] deptId
+     * @return boolean result Description
+     * @description หน้าบันทึกเงินจัดสรรสำหรับโครงการพัฒนามหาวิทยาลัย
+     */
+    public function updateProject($bgHeadId, $projectName, $budgetPeriodId, $budgetTotal, $deptId);
+
+    /**
+     * @name deleteProject
+     * @uri /deleteProject
+     * @param int bgHeadId
+     * @return boolean result Description
+     * @description หน้าเพิ่มเงินจัดสรรสำหรับโครงการพัฒนามหาวิทยาลัย
+     */
+    public function deleteProject($bgHeadId);
 }
--- ebudget/apps/revenue/service/PlaningService.php
+++ ebudget/apps/revenue/service/PlaningService.php
@@ -25,6 +25,23 @@ class PlaningService extends CServiceBase implements IPlaningService {
         return $this->datacontext->getObject($year)[0];
     }
 
+    public function listsDepartment() {
+        $repo = "SELECT"
+                ." dp.id, dp.deptName AS name, dp.masterId"
+            ." FROM ".$this->ent."\\L3D\\Department dp"
+            ." WHERE dp.deptStatus = :status"
+                ." AND dp.id > :id"
+                ." AND dp.deptGroup = :group";
+        $param = array(
+            "status" => "Y",
+            "id" => 0,
+            "group" => "A"
+        );
+        $data = $this->datacontext->getObject($repo, $param);
+
+        return $data;
+    }
+
     public function fetchRevenue() {
         $sql = "SELECT"
                 ." rp.id, rp.deptId, dp.deptName, rp.budgetEducation, rp.budgetService, rp.budgetTotal"
@@ -40,6 +57,38 @@ class PlaningService extends CServiceBase implements IPlaningService {
         return $data;
     }
 
+    public function addRevenue($revenuePlan) {
+        $return = true;
+
+        $revenuePlan->budgetPeriodId = $this->getPeriod()->year;
+        $revenuePlan->budgetTypeCode = "K";
+        $revenuePlan->budgetTotal = $revenuePlan->budgetEducation + $revenuePlan->budgetService;
+
+        if (!$this->datacontext->saveObject($revenuePlan)) {
+            $return = $this->datacontext->getLastMessage();
+        }
+
+        return $return;
+    }
+
+    public function updateRevenue($revenuePlan) {
+        $return = true;
+
+        $rp = new \apps\common\entity\BudgetRevenuePlan();
+        $rp->id = $revenuePlan->id;
+        $data = $this->datacontext->getObject($rp)[0];
+
+        $data->budgetEducation =  $revenuePlan->budgetEducation;
+        $data->budgetService =  $revenuePlan->budgetService;
+        $data->budgetTotal = $revenuePlan->budgetEducation + $revenuePlan->budgetService;
+
+        if (!$this->datacontext->updateObject($data)) {
+            return $this->datacontext->getLastMessage();
+        }
+
+        return $return;
+    }
+
     public function deleteRevenue($id) {
         $return = true;
 
@@ -50,5 +99,41 @@ class PlaningService extends CServiceBase implements IPlaningService {
         }
 
         return $return;
+    }
+
+    public function fetchProject() {
+        $sql = "SELECT"
+                ." bh.id,"
+                ." be.name AS projName,"
+                ." be.deptId,"
+                ." be.budgetEstAmount As deptValue"
+            ." FROM " . $this->ent . "\\BudgetHead bh"
+            ." JOIN " . $this->ent . "\\BudgetExpense be WITH bh.id = be.budgetHeadId"
+            ." WHERE bh.formId = :form"
+                ." AND bh.budgetTypeCode = :type"
+                ." AND bh.budgetPeriodId = :year"
+            ." ORDER BY bh.id, be.id";
+
+        $param = array(
+            "form" => "999",
+            "type" => "K",
+            "year" => $this->getPeriod()->year
+        );
+
+        $dataIAT = $this->datacontext->getObject($sql, $param);
+
+        return $dataIAT;
+    }
+
+    public function addProject($projectName, $budgetTotal, $deptId) {
+
+    }
+
+    public function updateProject($bgHeadId, $projectName, $budgetPeriodId, $budgetTotal, $deptId) {
+
+    }
+
+    public function deleteProject($bgHeadId) {
+
     }
 }
