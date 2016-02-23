a25a1d89eb70b7bdb6af0b5086f990d9aed04d0e
palida <ppuangpae@gmail.com>
2016-02-23 18:39:42 +0700

    ปรับการแสดงผลหมวดรายจ่าย

--- ebudget/apps/bginfo/interfaces/IBudgetTypeService.php
+++ ebudget/apps/bginfo/interfaces/IBudgetTypeService.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace apps\bginfo\interfaces;
 
 /**
@@ -7,24 +8,23 @@ namespace apps\bginfo\interfaces;
  * @description ระบบจัดการประเภทหมวดรายจ่าย
  */
 interface IBudgetTypeService {
-    
+
     /**
      * @name manage
      * @uri /manage
      * @description หน้าจัดการประเภทหมวดรายจ่าย
-     */ 
+     */
     public function viewManage();
-    
+
     /**
      * @name fetchBudgetType
      * @uri /fetchBudgetType
      * @param apps\common\entity\BudgetType pData Description
      * @description ดึงข้อมูลประเภทหมวดรายจ่าย
      * @return apps\common\entity\BudgetType dataList
-     */ 
+     */
     public function fetchBudgetType($pData);
-    
-    
+
     /**
      * @name saveBudgetType
      * @uri /saveBudgetType
@@ -32,17 +32,42 @@ interface IBudgetTypeService {
      * @param Interger editId Description
      * @description บันทึกข้อมูลประเภทหมวดรายจ่าย
      * @return apps\common\entity\BudgetType dataList
-     */ 
-    public function saveBudgetType($pData,$editId);
-    
+     */
+    public function saveBudgetType($pData, $editId);
+
     /**
      * @name delBudgetType
      * @uri /delBudgetType
      * @param apps\common\entity\BudgetType pData Description
      * @description ลบข้อมูลประเภทหมวดรายจ่าย
      * @return apps\common\entity\BudgetType dataList
-     */ 
+     */
     public function delBudgetType($pData);
-    
 
+    /**
+     * @name listAllBudget
+     * @uri /listAllBudget
+     * @param string type
+     * @description ดึงข้อมูลประเภทหมวดรายจ่าย
+     * @return String[] lists Description
+     */
+    public function listAllBudget($type);
+
+    /**
+     * @name addBudgetType
+     * @uri /addBudgetType
+     * @param apps\common\entity\BudgetType pData Description
+     * @description บันทึกข้อมูลประเภทหมวดรายจ่าย
+     * @return apps\common\entity\BudgetType dataList
+     */
+    public function addBudgetType($pData);
+    
+    /**
+     * @name updateBudgetType
+     * @uri /updateBudgetType
+     * @param apps\common\entity\BudgetType pData Description
+     * @description บันทึกข้อมูลประเภทหมวดรายจ่าย
+     * @return apps\common\entity\BudgetType dataList
+     */
+    public function updateBudgetType($pData);
 }
