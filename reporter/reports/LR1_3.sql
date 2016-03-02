30ebe6d938f895ce5ffe8ab200fa3048dd23d8fe
Panya Noipram <panya.n@baanprogram.com>
2016-03-02 10:05:03 +0700

    jasper

---
+++ reporter/reports/LR1_3.sql
@@ -0,0 +1,1 @@
+exec SP_LR1_3 '{{PERIOD_ID}}','{{BUDGET_TYPE}}','{{FACULTY_ID}}','{{DEPT_ID_START}}','{{DEPT_ID_END}}','{{PLAN_ID_START}}','{{PLAN_ID_END}}','{{FUND_ID_START}}','{{FUND_ID_END}}','{{CAT_ID}}'
